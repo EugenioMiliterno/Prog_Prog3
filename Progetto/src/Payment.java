5da104e27957c9e6bb63532c91828284e70ec860
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2023-10-28 20:54:21 +0200

    Update Payment.java

--- Progetto/src/Payment.java
+++ Progetto/src/Payment.java
@@ -2,23 +2,25 @@ package Progetto.src;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+//Interfaccia per gestire i pagamenti
 interface PaymentStrategy {
     void pay(BigDecimal amount);
 }
-
+//Classe per gestire il pagamento TRAMITE CARTA
 class CreditCardPaymentStrategy implements PaymentStrategy {
     private String name;
     private String cardNumber;
     private String cvv;
     private String expirationDate;
-
+    
+    //Costruttore
     public CreditCardPaymentStrategy(String name, String cardNumber, String cvv, String expirationDate) {
         this.name = name;
         this.cardNumber = cardNumber;
         this.cvv = cvv;
         this.expirationDate = expirationDate;
     }
-
+    //Metodi Getter
     String getName() {
         return name;
     }
@@ -31,7 +33,7 @@ class CreditCardPaymentStrategy implements PaymentStrategy {
     String getExpDate(){
         return expirationDate;
     }
-
+    //Metodo per controllare se una data viene dopo l'altr
     private boolean compareDate(LocalDate A, LocalDate B){
         if(A.compareTo(B) > 0){
             return true;
@@ -39,7 +41,7 @@ class CreditCardPaymentStrategy implements PaymentStrategy {
             return false;
         }
     }
-
+    //Metodo per il controllo del pagamento
     private boolean ValidPayment(String num,String exp, String CVV){
         LocalDate today = LocalDate.now();
         LocalDate expDate = LocalDate.parse(exp);
@@ -51,27 +53,31 @@ class CreditCardPaymentStrategy implements PaymentStrategy {
         }
         return esito;
     }
-
+    //;etodo per implementare il pagamento TRAMITE CARTA
     @Override
     public void pay(BigDecimal amount) {
         System.out.println("Paying " + amount + " with credit card " + cardNumber);
     }
 }
 
+//Classe per gestire il pagamento TRATMITE CONTANTI
 class CashPaymentStrategy implements PaymentStrategy {
     @Override
     public void pay(BigDecimal amount) {
         System.out.println("Paying " + amount + " in cash");
     }
 }
-
+//Classe pagamento, per gestire entrambi i metodi
 class Payment {
+    //Attributo che specifica quale metodo si è scelto
     private PaymentStrategy paymentStrategy;
 
+    //Costruttore
     public Payment(PaymentStrategy paymentStrategy) {
         this.paymentStrategy = paymentStrategy;
     }
 
+    //Metodo per il pagamento effettivo
     public void pay(BigDecimal amount) {
         paymentStrategy.pay(amount);
     }
