8307ba915b1bea95bd3896c1e33459b18dd883aa
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2023-10-28 20:50:24 +0200

    Update Menu.java

--- Progetto/src/Menu.java
+++ Progetto/src/Menu.java
@@ -1,32 +1,31 @@
 package Progetto.src;
 import java.util.ArrayList;
 
+//Classe per gestire il menù del ristorante
 public class Menu {
     private ArrayList<String> appetizers;
     private ArrayList<String> entrees;
     private ArrayList<String> desserts;
-    
+    //Inizializzazione del menù
     public void initAppetizers(){
         appetizers.add("Bruschetta");
         appetizers.add("Caprese");
         appetizers.add("Insalata");
         appetizers.add("Frittura");
     }
-
     public void initEntrees(){
         entrees.add("Spaghetti");
         entrees.add("Lasagne");
         entrees.add("Risotto");
         entrees.add("Pizza");
     }
-
     public void initDesserts(){
         desserts.add("Tiramisù");
         desserts.add("Panna cotta");
         desserts.add("Gelato");
         desserts.add("Torta della nonna");
     }
-
+    //Costruttore
     public Menu() {
         this.appetizers = new ArrayList<String>();
         this.entrees = new ArrayList<String>();
@@ -35,7 +34,7 @@ public class Menu {
         initEntrees();
         initDesserts();
     }
-    
+    //Function di stampa per la visualizzazione
     public void printMenu() {
         System.out.println("Appetizers:");
         for (String item : this.appetizers) {
