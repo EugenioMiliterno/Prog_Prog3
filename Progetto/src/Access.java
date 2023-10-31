fad881f0138e7c15fb577eba0042e10effba362b
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2023-10-31 14:25:21 +0100

    Update Access.java

--- Progetto/src/Access.java
+++ Progetto/src/Access.java
@@ -1,21 +1,26 @@
 package Progetto.src;
 
-//Classe Generica per gestire le due tipologie di accesso
 public class Access {
-    //Stato dell'accesso (Waiter/Admin)
     private AccessState state;
-    //Metodo che garantisce l'accesso tramite pw
+
     public Access(String pw) {
-        if(pw == "Studente")
+        if(pw == "Studente"){
             this.state = new WaiterAccess();
-        else if(pw == "Ciaramella")
+            System.out.println("Waiter access granted.");
+        }else if(pw == "Ciaramella"){
             this.state = new AdminAccess();
+            System.out.println("Admin access granted.");
+        }
     }
-    //Function per il setting dello stato d'accesso
+
     public void setState(AccessState state) {
         this.state = state;
     }
-    //Function per l'accesso effettivo
+
+    public AccessState getState() {
+        return this.state;
+    }
+
     public void grantAccess() {
         state.grantAccess();
     }
