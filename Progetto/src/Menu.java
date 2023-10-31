2b680d36ddcdd95c6ce02f00677500d9b7a8ced6
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2023-10-31 14:26:17 +0100

    Update Menu.java

--- Progetto/src/Menu.java
+++ Progetto/src/Menu.java
@@ -1,31 +1,32 @@
 package Progetto.src;
 import java.util.ArrayList;
 
-//Classe per gestire il menù del ristorante
 public class Menu {
-    private ArrayList<String> appetizers;
-    private ArrayList<String> entrees;
-    private ArrayList<String> desserts;
-    //Inizializzazione del menù
+    private static ArrayList<String> appetizers;
+    private static ArrayList<String> entrees;
+    private static ArrayList<String> desserts;
+    
     public void initAppetizers(){
         appetizers.add("Bruschetta");
         appetizers.add("Caprese");
         appetizers.add("Insalata");
         appetizers.add("Frittura");
     }
+
     public void initEntrees(){
         entrees.add("Spaghetti");
         entrees.add("Lasagne");
         entrees.add("Risotto");
         entrees.add("Pizza");
     }
+
     public void initDesserts(){
         desserts.add("Tiramisù");
         desserts.add("Panna cotta");
         desserts.add("Gelato");
         desserts.add("Torta della nonna");
     }
-    //Costruttore
+
     public Menu() {
         this.appetizers = new ArrayList<String>();
         this.entrees = new ArrayList<String>();
@@ -34,7 +35,7 @@ public class Menu {
         initEntrees();
         initDesserts();
     }
-    //Function di stampa per la visualizzazione
+    
     public void printMenu() {
         System.out.println("Appetizers:");
         for (String item : this.appetizers) {
@@ -51,4 +52,7 @@ public class Menu {
             System.out.println("- " + item);
         }
     }
+    static ArrayList<String> getEntrees(){return entrees;}
+    static ArrayList<String> getAppetizers(){return appetizers;}
+    static ArrayList<String> getDesserts(){return desserts;}
 }
