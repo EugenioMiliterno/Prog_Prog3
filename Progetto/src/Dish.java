eaf91248163f3d435b9bd4732bc462dbbc743f20
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2023-10-31 14:25:41 +0100

    Update Dish.java

--- Progetto/src/Dish.java
+++ Progetto/src/Dish.java
@@ -1,27 +1,30 @@
 package Progetto.src;
+
 import java.util.Observable;
 import java.util.Observer;
 
-//Classe Dish per gestire un piatto del menù, usando il pattern Observable
 public class Dish extends Observable {
+    
     private String name;
     private double price;
-    //Costruttore del piatto
+
     public Dish(String name, double price) {
         this.name = name;
         this.price = price;
     }
-    //Metodi getter
+
     public String getName() {
         return name;
     }
+
     public double getCost() {
         return price;
     }
-    //Metodi Setter
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
