085f58a88466041b2d9e9a1d85db49205c870212
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2024-02-06 16:51:35 +0100

    Create Order.java

---
+++ src/Order.java
@@ -0,0 +1,40 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Order implements Observer {
+    private List<Dish> dishes;
+    private double totalCost;
+    public Order() {
+        dishes = new ArrayList<>();
+        totalCost = 0.0;
+    }
+    public int getOrdSize(){
+        return this.dishes.size();
+    }
+    public Dish getDish(int index) {
+        return dishes.get(index);
+    }
+    public void addDish(Dish dish) {
+        dishes.add(dish);
+        dish.addObserver(this);
+        totalCost += dish.getCosto();
+    }
+    public void removeDish(Dish dish) {
+        dishes.remove(dish);
+        dish.deleteObserver(this);
+        totalCost -= dish.getCosto();
+    }
+    public double getTotalCost() {
+        return totalCost;
+    }
+    @Override
+    public void update(Observable o, Object arg) {
+        if (arg instanceof Double) {
+            totalCost += (Double) arg;
+        }
+    }
+}
